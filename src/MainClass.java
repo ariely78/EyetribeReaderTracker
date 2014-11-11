6848256c1eb4dd70edc3094d3583b7afd8a7b221
Benjamin Terry Smith <benjamin@afrogleap.com>
2014-11-11 18:12:10 +0100

    added simple drawing and tracking

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,14 +1,7 @@
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
 import javax.swing.JFrame;
-
-import com.theeyetribe.client.*;
-import com.theeyetribe.client.GazeManager.ApiVersion;
-import com.theeyetribe.client.GazeManager.ClientMode;
-import com.theeyetribe.client.data.GazeData;
-import com.theeyetribe.client.GazeManager;
 
 public class MainClass {
     public static void main(String[] args)
@@ -19,33 +12,10 @@ public class MainClass {
     	frame.setSize(dim.width, dim.height);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	frame.setVisible(true);
-    	
-        final GazeManager gm = GazeManager.getInstance();
-        boolean success = gm.activate(ApiVersion.VERSION_1_0, ClientMode.PUSH);
-        
-        final GazeListener gazeListener = new GazeListener();
-        gm.addGazeListener(gazeListener);
-        
-        //TODO: Do awesome gaze control wizardry
-        
-        Runtime.getRuntime().addShutdownHook(new Thread()
-        {
-            @Override
-            public void run()
-            {
-                gm.removeGazeListener(gazeListener);
-             gm.deactivate();
-            }
-        });
+    	DragCircle d = new DragCircle();
+    	frame.add(d);
     }
 
 
-    private static class GazeListener implements IGazeListener
-    {
-        public void onGazeUpdate(GazeData gazeData)
-        {
-            System.out.println(gazeData.toString());
-        }
 
-    }
 }
