38a7d8ca486c061d9da87f079edc05428ccb92ae
ben smith <benjaminsmith1981@googlemail.com>
2014-11-18 20:29:43 +0100

    tracking refined

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,16 +1,7 @@
-
-import java.awt.CardLayout;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.io.*;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class MainClass {
     public static void main(String[] args) throws IOException
@@ -25,8 +16,6 @@ public class MainClass {
     	
         final WordSelection txtContent = new WordSelection(DocumentReader.readTextFile("text.txt"));
     	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        txtContent.setSize(300,400);
-        txtContent.setEditable(false);
         JFrame frame = new JFrame();
         frame.setSize(dim.width, dim.height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/WordSelection.java
+++ src/WordSelection.java
@@ -10,6 +10,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import com.sun.javafx.geom.Point2D;
+import com.sun.jmx.snmp.Timestamp;
 import com.theeyetribe.client.IGazeListener;
 import com.theeyetribe.client.GazeManager;
 import com.theeyetribe.client.GazeManager.ApiVersion;
@@ -20,22 +21,73 @@ import com.theeyetribe.client.IGazeListener;
 /**
  * Gets word from right clicked area
  */
-public class WordSelection extends JTextArea {
+public class WordSelection extends JPanel {
     private int x;
     private int y;
+    int textareaX = 0;
+    int textareaY = 0;
+
     JTextArea txtContent;
-    
+    private Point last;
+	private long lastTimeStamp = 0;
+
     public WordSelection(String text){
-	    this.setEditable(false);
-	    this.setText(text);
-    	txtContent = this;
-    	Font font = new Font("Verdana", Font.BOLD, 20);
-    	this.setFont(font);
-    	this.setBounds(100, 0, 300, 500);
+    	last = new Point(0,0);
+    	txtContent =  new JTextArea()
+    	  {
+            @Override
+            protected void paintComponent(Graphics g)
+            {
+              super.paintComponent(g);
+              g.setColor(g.getColor().RED);
+              g.fillOval(x, y, 25, 25);
+//              g.drawLine(last.x, last.y, x, y);
+            }   
+      };
+    	txtContent.setEditable(false);
+    	txtContent.setText(text);
+    	Font font = new Font("Verdana", Font.BOLD, 30);
+    	txtContent.setFont(font);
+    	txtContent.setBounds(textareaX, 0, 300, 500);
     	
-//    	this.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
+    	txtContent.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent m) {
+                int dx = m.getX() - last.x;
+                int dy = m.getY() - last.y;
+                x += dx;
+                y += dy;
+                last = m.getPoint();
+            	txtContent.setCaretPosition(txtContent.viewToModel(m.getPoint()));
+	            int caretPosition = txtContent.getCaretPosition();
+	            try {
+	                String word = getWord(caretPosition, txtContent);
+	                if((System.currentTimeMillis() - lastTimeStamp) > 500){
+	                	lastTimeStamp = System.currentTimeMillis();
+//	                	JOptionPane.showMessageDialog(null, "Word: "+word + " Letter:" +txtContent.getText(caretPosition-1,1));
+	                	DocumentReader.writeToTextFile("output.txt", word+"  "+ txtContent.getText(caretPosition-1,1));
+	                }
+	            } catch (BadLocationException e1) {
+	                e1.printStackTrace();
+	            }
+	            txtContent.repaint(0,0,300,100);
+            }
+    	});
+    	txtContent.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent m) {
+                last = m.getPoint();
+                x = last.x;
+                y = last.y;
+
+                txtContent.repaint();
+            }
+
+
+            
+            @Override
+            public void mouseClicked(MouseEvent e) {
 //            	txtContent.setCaretPosition(txtContent.viewToModel(e.getPoint()));
 //                int caretPosition = txtContent.getCaretPosition();
 //                try {
@@ -45,9 +97,10 @@ public class WordSelection extends JTextArea {
 //                } catch (BadLocationException e1) {
 //                    e1.printStackTrace();
 //                }
-//            }
-//        });
-        
+            }
+        });
+    	this.add(txtContent);
+
         final GazeManager gm = GazeManager.getInstance();
         boolean success = gm.activate(ApiVersion.VERSION_1_0, ClientMode.PUSH);
         
@@ -72,44 +125,47 @@ public class WordSelection extends JTextArea {
 	    this.setLayout(new GridBagLayout());
 
 	    for (int i = 0; i < 1; i++) {
-	      this.setEditable(false);
-
-	      this.setText(text);
-
-	      GridBagConstraints constraints = new GridBagConstraints();
-	      constraints.gridx = 0;
-	      constraints.gridy = i;
-	      constraints.fill = GridBagConstraints.VERTICAL;
-	      constraints.weightx = 1.0;
-	      constraints.insets.bottom = 5;
-
-	      this.add(this, constraints);
+			txtContent.setEditable(false);
+			txtContent.setText(text);
+			
+			GridBagConstraints constraints = new GridBagConstraints();
+			constraints.gridx = 0;
+			constraints.gridy = i;
+			constraints.fill = GridBagConstraints.VERTICAL;
+			constraints.weightx = 1.0;
+			constraints.insets.bottom = 5;
+			this.add(txtContent, constraints);
 	    }
 	}
-	  
+	
+
     @SuppressWarnings("unused")
 	private class GazeListener implements IGazeListener
     {
-
         public void onGazeUpdate(GazeData gazeData)
         {   
-            System.out.println(gazeData.smoothedCoordinates.x);
-            System.out.println(gazeData.smoothedCoordinates.y);
-
-            x = (int)gazeData.smoothedCoordinates.x;
-            y = (int)gazeData.smoothedCoordinates.y;
+            x = (int)gazeData.smoothedCoordinates.x-textareaX;
+            y = (int)gazeData.smoothedCoordinates.y-textareaY;
+        
             
-//            this.setCaretPoint(gazeData.smoothedCoordinates);
+            if((gazeData.timeStamp - lastTimeStamp) > 0){
+            	 txtContent.repaint(x-25,y-25,50,50);
+            	lastTimeStamp = gazeData.timeStamp;
+//                this.setCaretPoint(gazeData);
+            }
         }
         
-        public void setCaretPoint(Point2D pointGaze)
+        public void setCaretPoint(GazeData gazeData)
         {
-        	Point pt = new Point((int)pointGaze.x, (int)pointGaze.y);
+        	Point pt = new Point((int)gazeData.smoothedCoordinates.x-textareaX, 
+        						(int)gazeData.smoothedCoordinates.y-textareaY);
         	txtContent.setCaretPosition(txtContent.viewToModel(pt));
             int caretPosition = txtContent.getCaretPosition();
             try {
                 String word = getWord(caretPosition, txtContent);
-                DocumentReader.writeToTextFile("output.txt", word+"  "+ txtContent.getText(caretPosition-1,1));
+                DocumentReader.writeToTextFile("output.txt", word+"  "
+                				+ txtContent.getText(caretPosition-1,1)
+                				+ " " +gazeData.timeStampString);
             } catch (BadLocationException e1) {
                 e1.printStackTrace();
             }
@@ -136,5 +192,11 @@ public class WordSelection extends JTextArea {
         }
         startIndex = caretPosition - j;
         return txtContent.getText(startIndex, endIndex - startIndex);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawOval(x, y, 5, 5);
     }
 }
